721a0f597bc76648b3b4467dec4bbb02c3d3b723
Isabela Müller <isabelamuller1@hotmail.com>
2021-11-23 22:32:57 -0300

    oi de novo
    fiz pequenas arrumacoes apenas amigos!!!!!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,13 @@
- import java.util.Scanner;
 
-    public class Main {
-        public static void main(String[] args) {
-            int[] arr = {1, 4, 5, 6, 4, 8, 9, 9};
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 5, 6, 4, 8, 9, 9};
 
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] == 9) {
-                    System.out.println("encontrei o 9 no index " + i);
-                }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 9) {
+                System.out.println("encontrei o 9 no index " + i);
             }
         }
     }
-    // criou uma array de numeros e quer saber em qual index ta um numero x
 }
+// criou uma array de numeros e quer saber em qual index ta um numero x
